ebe8d372d53f16d1c41786aa42582374e8ae337e
Nguyễn Đức Toàn <toan.nd235846@sis.hust.edu.vn>
2026-03-27 13:59:36 +0700

    Complete assignment 1.1.2

---
+++ Lab1/Code/FirstDialog.java
@@ -0,0 +1,14 @@
+package Lab1;
+
+//Example 2:FirstDialog.java
+import javax.swing.JOptionPane;
+public class FirstDialog {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null,"Hello world! How are you?");
+		System.exit(0);
+
+	}
+
+}
+
